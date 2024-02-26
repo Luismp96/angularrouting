037691a253124afd5f7d38304969ea1f5f74b5bb
= <=>
2024-02-26 19:54:42 +0100

    Enroutamiento

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//SE AÑADEN RUTAS
+import { InicioComponent } from './inicio/inicio.component';
+import { BlogComponent } from './blog/blog.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  //SE AÑADEN RUTAS
+  //INICIO
+  {
+    path:'inicio',component:InicioComponent
+  },
+  {
+    path:'blog',component:BlogComponent
+  },
+  {
+    path:'sobremi',component:SobremiComponent
+  },
+  {
+    path:'contacto',component:ContactoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
